f605ccffac8ad7d806a9b2e314442599b84f92c0
Tomasz Borychowski <tomasz.borychowski@sap.com>
2018-12-06 17:35:33 +0000

    some keyboard

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # NOTER
 A [nordically](https://github.com/arcticicestudio/nord) themed note taking app.
+
+
+
+### Todo
+- [ ] "n" key to add a new note
+- [ ] "j", "k" keys to move to the next/prev note
+- [ ] arrow keys to move around - left/right to move between folders/notes, up/down to move respectively
+- [ ] enter to open the note
--- client/data/store.js
+++ client/data/store.js
@@ -1,8 +1,16 @@
 import { Store } from 'svelte/store.js';
 
-export default new Store({
+const store = new Store({
 	notes: [],
 	folder: '',
 	note: null,
 });
 
+
+store.compute('notesInFolder', ['folder', 'notes'], (folder, notes) => {
+	if (folder) notes = notes.filter(n => n.folder === folder);
+	return notes.sort((a, b) => a.title.localeCompare(b.title));
+});
+
+
+export default store;
